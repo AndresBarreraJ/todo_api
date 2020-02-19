bd637a56a48415025b57b9112179f2da0b858de6
AndresBarreraJ <andresbarreraj@gmail.com>
2020-02-19 12:27:06 -0700

    part 2

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,14 +1,30 @@
 module ExceptionHandler
   #provides the more graceful included method
   extend ActiveSupport::Concern
+  #define custom error subclasses - rescue catchers 'StandardErrors'
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
 
   included do
+    #define custom handlers
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({message: e.message}, :not_found)
     end
+  end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({message: e.message}, :unprocessable_entity)
-    end
+  private
+  #JSOn response with code 422
+  def four_twenty_two(e)
+    json_response({message: e.message}, :unprocessable_entity)
+  end
+#json response with code 401
+  def unauthorized_request(e)
+    json_response({message: e.message}, :unauthorized)
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,7 +19,8 @@ end
 RSpec.configure do |config|
   # [...]
   # add `FactoryBot` methods
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   config.include FactoryBot::Syntax::Methods
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
